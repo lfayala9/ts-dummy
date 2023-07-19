15df1c889500312ac791f9dda53db0b033ed951a
Luis Felipe <lfayala9@gmail.com>
2023-07-18 21:16:03 -0400

    first deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Welcome from './pages/Welcome.tsx'
-import { SignIn } from './pages/SignIn.tsx'
 
 const App = (): JSX.Element => {
   return (
