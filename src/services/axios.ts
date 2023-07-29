c16e6fab011c7998af92335ea977b9f56e4e09d0
Luis Felipe <lfayala9@gmail.com>
2023-07-29 00:05:02 -0400

    fixed bugs

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create()
-axiosInstance.defaults.baseURL = process.env.REACT_APP_API
+const API = process.env.REACT_APP_API
+axiosInstance.defaults.baseURL = API
 
 export default axiosInstance
