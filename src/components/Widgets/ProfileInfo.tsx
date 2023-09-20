d221f7d9636fa56cc389f3f518a4554355cda2bc
Luis Felipe <lfayala9@gmail.com>
2023-09-20 11:38:54 -0400

    Minor improvements

---
+++ src/components/Widgets/ProfileInfo.tsx
@@ -0,0 +1,67 @@
+import { Box, Avatar, Typography, IconButton, Divider, FormHelperText } from '@mui/material'
+import { Wrapper } from '../../styles/components'
+import { type UserInfo } from '../../types'
+import { useAppSelector } from '../../utils/hooks/selector'
+import EditIcon from '@mui/icons-material/Edit'
+
+const ProfileInfo = ({ user }: { user: UserInfo }): JSX.Element => {
+  const { theme } = useAppSelector((state) => state.settings)
+  const date = new Date(user?.createdAt)
+
+  return (
+    <>
+      <Box display="flex" flexDirection="column" position="fixed">
+        <Box display="flex" alignItems="center" height="170px" gap="15px">
+          <Avatar
+            sx={{
+              width: '150px',
+              height: '150px',
+              border: '2px solid white'
+            }}
+          >
+            <img
+              width={190}
+              height={'auto'}
+              src={user?.picture}
+              alt="user-picture"
+            />
+          </Avatar>
+          <Typography variant="h2">
+            {user?.firstName} {user?.lastName}
+          </Typography>
+        </Box>
+        <Wrapper sx={{ p: 0 }}>
+          <Typography
+            variant="h3"
+            fontWeight="bold"
+            sx={{
+              backgroundColor: theme === 'light' ? '#808080' : '#171717',
+              px: 3,
+              py: 1.3,
+              borderRadius: '2rem 2rem 0rem 0rem'
+            }}
+          >
+            User Description
+          </Typography>
+          <Box p={2}>
+            <Box
+              display="flex"
+              justifyContent={'space-between'}
+              mb={1}
+              alignItems="center"
+            >
+              <Typography>Add Description</Typography>
+              <IconButton>
+                <EditIcon sx={{ width: '15px' }} />
+              </IconButton>
+            </Box>
+            <Divider/>
+            <FormHelperText sx={{ mt: 1 }}>Joined In {date.toDateString()}</FormHelperText>
+          </Box>
+        </Wrapper>
+      </Box>
+    </>
+  )
+}
+
+export default ProfileInfo
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,5 @@
 import React, { useEffect, useState, lazy, Suspense } from 'react'
-import { Wrapper } from '../styles/components'
-import {
-  Grid,
-  CssBaseline,
-  Box,
-  Avatar,
-  Typography,
-  FormHelperText,
-  Divider
-} from '@mui/material'
+import { Grid, CssBaseline } from '@mui/material'
 import { useAppSelector } from '../utils/hooks/selector'
 import { useParams } from 'react-router-dom'
 import { getUser } from '../utils/hooks/useGetUser'
@@ -18,11 +9,11 @@ import { TabTitle } from '../utils/hooks/titles'
 import PostsList from '../components/Posts/PostsList'
 import LoaderRing from '../components/Widgets/Loader'
 import { useDisplay } from '../utils/hooks/display'
+import ProfileInfo from '../components/Widgets/ProfileInfo'
 const AddUser = lazy(async () => await import('../components/AddBox/AddUser'))
 
 const Profile: React.FC = () => {
   const { token } = useAppSelector((state) => state.auth)
-  const { theme } = useAppSelector((state) => state.settings)
   const { userId } = useParams()
   const { isDisplayNone } = useDisplay()
 
@@ -39,7 +30,6 @@ const Profile: React.FC = () => {
       user != null ? user.lastName : ''
     }`
   )
-  const date = new Date(user?.createdAt as string)
   return (
     <>
       <Helmet>
@@ -51,58 +41,28 @@ const Profile: React.FC = () => {
           item
           className="start-section"
           justifyContent="end"
-          md={5}
+          md={6}
           lg={3.5}
         >
           {isDisplayNone
             ? null
-            : <Box display="flex" flexDirection="column">
-            <Box display="flex" alignItems="center" height="170px" gap="15px">
-              <Avatar
-                sx={{
-                  width: '150px',
-                  height: '150px',
-                  border: '2px solid white'
-                }}
-              >
-                <img width={150} height={'auto'} src={user?.picture} />
-              </Avatar>
-              <Typography variant="h2">
-                {user?.firstName} {user?.lastName}
-              </Typography>
-            </Box>
-            <Wrapper sx={{ p: 0 }}>
-              <Typography
-                variant="h3"
-                fontWeight="bold"
-                sx={{
-                  backgroundColor: theme === 'light' ? '#808080' : '#171717',
-                  px: 3,
-                  py: 1.3,
-                  borderRadius: '2rem 2rem 0rem 0rem'
-                }}
-              >
-                User Description
-              </Typography>
-              <Box p={2}>
-                <Typography mb={2}>Add Description</Typography>
-                <Divider />
-                <FormHelperText>Joined In {date.toDateString()}</FormHelperText>
-              </Box>
-            </Wrapper>
-          </Box>}
+            : (
+              <ProfileInfo user={user as UserInfo}/>
+              )}
         </Grid>
-        <Grid item xs={12} md={7} lg={5.5} px={2} className="main-section">
+        <Grid item xs={12} md={6} lg={5.5} px={2} className="main-section">
           <PostsList isHome={false} token={token} userId={userId} />
         </Grid>
         <Grid item className="end-section" lg={3} md={0}>
           {isDisplayNone
             ? null
-            : <Suspense
-            fallback={<LoaderRing position="relative" top="10%" left="30%" />}
-          >
-            <AddUser isProfile={true} id={userId}/>
-          </Suspense>}
+            : (
+            <Suspense
+              fallback={<LoaderRing position="relative" top="10%" left="30%" />}
+            >
+              <AddUser isProfile={true} id={userId} />
+            </Suspense>
+              )}
         </Grid>
       </Grid>
     </>
